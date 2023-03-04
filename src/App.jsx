12e00b77b51e4ca1702fabdc3d24ab226605980f
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-03-04 12:57:03 +0100

    iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ function App() {
 
   const [showCounter, setShowCounter] = useState(true)
 
+  const handleCounter = () => {
+    setShowCounter(prev => !prev)
+  }
+
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <button className='show-counter' onClick={handleCounter}>{showCounter ? 'Hide counter' : 'Show counter'}</button>
       {showCounter && <Counter />}
     </div>
   );
